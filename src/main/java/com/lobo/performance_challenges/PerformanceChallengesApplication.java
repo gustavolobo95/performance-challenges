3dd8c1f14ea19f9d5f568108d7ddd06cb47fafc6
Gustavo Lobo <gustavo.lobo95@gmail.com>
2026-08-23 19:14:25 -0300

    Inicializando projeto

---
+++ src/main/java/com/lobo/performance_challenges/PerformanceChallengesApplication.java
@@ -0,0 +1,13 @@
+package com.lobo.performance_challenges;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerformanceChallengesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerformanceChallengesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lobo/performance_challenges/PerformanceChallengesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lobo.performance_challenges;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerformanceChallengesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
